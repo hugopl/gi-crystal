0da88e6e02401bf93b4ab276d578f16529fab15b
Hugo Parente Lima <hugo.pl@gmail.com>
2021-09-12 16:44:58 -0300

    Useless refactor on interface generator.

--- src/generator/interface_wrapper_generator.cr
+++ src/generator/interface_wrapper_generator.cr
@@ -3,27 +3,31 @@ module Generator
     include WrapperUtil
 
     @namespace : Namespace
-    @iface_info : InterfaceInfo
+    @iface : InterfaceInfo
 
-    def initialize(@namespace : Namespace, @iface_info : InterfaceInfo)
+    def initialize(@namespace : Namespace, @iface : InterfaceInfo)
     end
 
     def filename : String?
-      "#{@iface_info.name.underscore}.cr"
+      "#{@iface.name.underscore}.cr"
     end
 
     def subject : String
-      @iface_info.name
+      @iface.name
     end
 
     private def do_generate(io : IO)
-      iface_name = to_lib_type(@iface_info, include_namespace: false)
+      iface_name = to_lib_type(@iface, include_namespace: false)
       io << "module " << to_type_name(@namespace.name) << LF
-      doc_repo.doc(io, @iface_info)
+      doc_repo.doc(io, @iface)
       io << "module " << iface_name << LF
-      MethodWrapperGenerator.generate(io, @iface_info.methods)
-      PropertyWrapperGenerator.generate(io, @iface_info.properties)
+      generate_prerequisites(io)
+
+      MethodWrapperGenerator.generate(io, @iface.methods)
+      PropertyWrapperGenerator.generate(io, @iface.properties)
+
       io << "end\n\n"
+
       io << "# :nodoc:\n"
       io << "class " << iface_name << "__Impl\n"
       io << "  include " << iface_name << "\n"
@@ -46,6 +50,16 @@ module Generator
 
       io << "end\n" \
             "end\n"
+    end
+
+    private def generate_prerequisites(io : IO)
+      namespace = @iface.namespace
+      @iface.prerequisites.each do |iface|
+        include_namespace = iface.namespace != namespace
+
+        io << "# Pre requisite: " << to_crystal_type(iface, include_namespace: include_namespace) << LF
+      end
+      io << LF
     end
   end
 end
--- src/gobject_introspection/interface_info.cr
+++ src/gobject_introspection/interface_info.cr
@@ -6,12 +6,24 @@ module GObjectIntrospection
     include FunctionInfoContainer
     include PropertyInfoContainer
 
+    @prerequisites : Array(InterfaceInfo)?
+
     def methods : Array(FunctionInfo)
       methods(->LibGIRepository.g_interface_info_get_n_methods, ->LibGIRepository.g_interface_info_get_method)
     end
 
     def properties : Array(PropertyInfo)
       properties(->LibGIRepository.g_interface_info_get_n_properties, ->LibGIRepository.g_interface_info_get_property)
+    end
+
+    def prerequisites : Array(InterfaceInfo)
+      @prerequisites ||= begin
+        n = LibGIRepository.g_interface_info_get_n_prerequisites(self)
+        Array.new(n) do |i|
+          ptr = LibGIRepository.g_interface_info_get_prerequisite(self, i)
+          InterfaceInfo.new(ptr)
+        end
+      end
     end
   end
 end
