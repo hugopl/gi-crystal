17c1ce538894b472b48cc97b5e4c9f59ae7296c7
Hugo Parente Lima <hugo.pl@gmail.com>
2022-03-10 23:27:35 -0300

    Use custom unref methods if needed.

--- src/gobject_introspection/object_info.cr
+++ src/gobject_introspection/object_info.cr
@@ -17,6 +17,11 @@ module GObjectIntrospection
       ObjectInfo.new(ptr) if ptr
     end
 
+    def unref_function : String
+      func = LibGIRepository.g_object_info_get_unref_function(self)
+      func.null? ? "g_object_unref" : String.new(func)
+    end
+
     def methods : Array(FunctionInfo)
       methods(->LibGIRepository.g_object_info_get_n_methods, ->LibGIRepository.g_object_info_get_method)
     end
