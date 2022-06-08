99082a8f8019ac2121d4707eac99b615676505bb
David Keller <davidkeller@tuta.io>
2022-06-08 20:12:20 +0200

    Fix annotation

--- src/bindings/g_object/object.cr
+++ src/bindings/g_object/object.cr
@@ -1,111 +1,113 @@
-class GObject::Object
+module GObject
   # :nodoc:
   annotation GeneratedWrapper
     # This annotation is used to identify user types that inherit from GObject from binding types that does the same.
   end
 
-  # Declares a GObject signal.
-  #
-  # Supported signal parameter types are:
-  #
-  #  - Integer types
-  #  - Float types
-  macro signal(signature)
-    {%
-    raise "signal parameter must be a signature (Macros::Call), got #{signature.class_name}" unless signature.is_a?(Call)
-    raise "Signal signature #{signature} can't have a receiver" if signature.receiver
-    raise "Signal signature #{signature} can't have a block argument" if signature.block_arg
-    %}
+  class Object
+    # Declares a GObject signal.
+    #
+    # Supported signal parameter types are:
+    #
+    #  - Integer types
+    #  - Float types
+    macro signal(signature)
+      {%
+      raise "signal parameter must be a signature (Macros::Call), got #{signature.class_name}" unless signature.is_a?(Call)
+      raise "Signal signature #{signature} can't have a receiver" if signature.receiver
+      raise "Signal signature #{signature} can't have a block argument" if signature.block_arg
+      %}
 
-    struct {{ signature.name.titleize }}Signal < GObject::Signal
-      def name : String
-        @detail ? "{{ signature.name }}::#{@detail}" : {{ signature.name.stringify }}
-      end
+      struct {{ signature.name.titleize }}Signal < GObject::Signal
+        def name : String
+          @detail ? "{{ signature.name }}::#{@detail}" : {{ signature.name.stringify }}
+        end
 
-      alias LeanProc = Proc({{ (signature.args.map(&.type) << Nil).splat }})
+        alias LeanProc = Proc({{ (signature.args.map(&.type) << Nil).splat }})
 
-      def connect(*, after : Bool = false, &block : LeanProc) : GObject::SignalConnection
-        connect(block, after: after)
-      end
+        def connect(*, after : Bool = false, &block : LeanProc) : GObject::SignalConnection
+          connect(block, after: after)
+        end
 
-      def connect(handler : LeanProc, *, after : Bool = false) : GObject::SignalConnection
-        _box = ::Box.box(handler)
-        {% if signature.args.empty? %}
-        handler = ->(_lib_sender : Pointer(Void), _lib_box : Pointer(Void)) { ::Box(LeanProc).unbox(_lib_box).call }.pointer
-        {% else %}
-        handler = ->(_lib_sender : Pointer(Void),
-        {% for arg in signature.args %}
-        {%
-          resolved_type = arg.type.resolve
-          if resolved_type == String
-            type = ::Pointer(UInt8)
-          elsif resolved_type == Bool
-            type = ::Int32
-          else
-            type = arg.type
-          end
-        %}
-        {{ arg.var }} : {{ type }},
-        {% end %}
-        _lib_box : Pointer(Void)) {
-
+        def connect(handler : LeanProc, *, after : Bool = false) : GObject::SignalConnection
+          _box = ::Box.box(handler)
+          {% if signature.args.empty? %}
+          handler = ->(_lib_sender : Pointer(Void), _lib_box : Pointer(Void)) { ::Box(LeanProc).unbox(_lib_box).call }.pointer
+          {% else %}
+          handler = ->(_lib_sender : Pointer(Void),
           {% for arg in signature.args %}
-          {% resolved_type = arg.type.resolve %}
-          {% if arg.type.resolve == String %}
-            {{ arg.var }} = String.new({{ arg.var }})
-          {% elsif arg.type.resolve == Bool %}
-            {{ arg.var }} = {{ arg.var }} != 0
-          {% end %}
-          {% end %}
-
-          ::Box(LeanProc).unbox(_lib_box).call({{ signature.args.map(&.var).splat }})
-        }.pointer
-        {% end %}
-
-        handler = LibGObject.g_signal_connect_data(@source, name, handler,
-          GICrystal::ClosureDataManager.register(_box), ->GICrystal::ClosureDataManager.deregister, after.to_unsafe)
-        GObject::SignalConnection.new(@source, handler)
-      end
-
-      def emit({{ signature.args.splat }})
-        LibGObject.g_signal_emit_by_name(@source, {{ signature.name.stringify }}, {{ signature.args.map(&.var).splat }})
-      end
-    end
-
-    def {{ signature.name }}_signal
-      {{ signature.name.titleize }}Signal.new(self)
-    end
-
-    def self._class_init(klass : Pointer(LibGObject::TypeClass), user_data : Pointer(Void)) : Nil
-      LibGObject.g_signal_new({{ signature.name.stringify }}, g_type,
-      GObject::SignalFlags.flags(RunLast, NoRecurse, NoHooks),
-      0,                   # class_offset
-      Pointer(Void).null,  # accumulator
-      Pointer(Void).null,  # accumulator_data
-      Pointer(Void).null,  # marshaller
-      GObject::TYPE_NONE,  # return_type
-      {{ signature.args.size }}, # n_params
-      {% for arg in signature.args %}
-      {{ arg.type }}.g_type,
-      {% end %}
-      Pointer(Void).null)
-      previous_def
-    end
-  end
-
-  macro inherited
-    {% unless @type.annotation(GObject::GeneratedWrapper) %}
-      macro method_added(method)
-        {% verbatim do %}
-          {% if method.name.starts_with?("vfunc_") && method.name.size > 6 %}
-            {% function_name = method.name[6..] %}
-            def self._class_init(klass : Pointer(LibGObject::TypeClass), user_data : Pointer(Void)) : Nil
-              self._class_init_vfunc_{{function_name}}(klass, user_data)
-              previous_def
+          {%
+            resolved_type = arg.type.resolve
+            if resolved_type == String
+              type = ::Pointer(UInt8)
+            elsif resolved_type == Bool
+              type = ::Int32
+            else
+              type = arg.type
             end
+          %}
+          {{ arg.var }} : {{ type }},
           {% end %}
-        {% end %}
+          _lib_box : Pointer(Void)) {
+
+            {% for arg in signature.args %}
+            {% resolved_type = arg.type.resolve %}
+            {% if arg.type.resolve == String %}
+              {{ arg.var }} = String.new({{ arg.var }})
+            {% elsif arg.type.resolve == Bool %}
+              {{ arg.var }} = {{ arg.var }} != 0
+            {% end %}
+            {% end %}
+
+            ::Box(LeanProc).unbox(_lib_box).call({{ signature.args.map(&.var).splat }})
+          }.pointer
+          {% end %}
+
+          handler = LibGObject.g_signal_connect_data(@source, name, handler,
+            GICrystal::ClosureDataManager.register(_box), ->GICrystal::ClosureDataManager.deregister, after.to_unsafe)
+          GObject::SignalConnection.new(@source, handler)
+        end
+
+        def emit({{ signature.args.splat }})
+          LibGObject.g_signal_emit_by_name(@source, {{ signature.name.stringify }}, {{ signature.args.map(&.var).splat }})
+        end
       end
-    {% end %}
+
+      def {{ signature.name }}_signal
+        {{ signature.name.titleize }}Signal.new(self)
+      end
+
+      def self._class_init(klass : Pointer(LibGObject::TypeClass), user_data : Pointer(Void)) : Nil
+        LibGObject.g_signal_new({{ signature.name.stringify }}, g_type,
+        GObject::SignalFlags.flags(RunLast, NoRecurse, NoHooks),
+        0,                   # class_offset
+        Pointer(Void).null,  # accumulator
+        Pointer(Void).null,  # accumulator_data
+        Pointer(Void).null,  # marshaller
+        GObject::TYPE_NONE,  # return_type
+        {{ signature.args.size }}, # n_params
+        {% for arg in signature.args %}
+        {{ arg.type }}.g_type,
+        {% end %}
+        Pointer(Void).null)
+        previous_def
+      end
+    end
+
+    macro inherited
+      {% unless @type.annotation(GObject::GeneratedWrapper) %}
+        macro method_added(method)
+          {% verbatim do %}
+            {% if method.name.starts_with?("vfunc_") && method.name.size > 6 %}
+              {% function_name = method.name[6..] %}
+              def self._class_init(klass : Pointer(LibGObject::TypeClass), user_data : Pointer(Void)) : Nil
+                self._class_init_vfunc_{{function_name}}(klass, user_data)
+                previous_def
+              end
+            {% end %}
+          {% end %}
+        end
+      {% end %}
+    end
   end
 end
