94217aefd1f0551e95c85cf5e0ab091290e655a4
David Keller <davidkeller@tuta.io>
2022-06-09 19:50:40 +0200

    Move `GObject::Object`, `LibGLib` and `LibGObject` only methods away from .ecr files (#15)

---
+++ src/bindings/g_lib/lib_g_lib.cr
@@ -0,0 +1,21 @@
+lib LibGLib
+  # Functions not declared by GObj Introspection
+
+  # Memory related functions
+  fun g_malloc0(size : LibC::SizeT) : Void*
+  fun g_free(mem : Void*)
+
+  # GList
+  fun g_list_length(list : List*) : UInt32
+  fun g_list_free(list : List*)
+  fun g_list_free_full(list : List*, free_func : Proc(Void*, Nil))
+  fun g_list_first(list : List*) : List*
+  fun g_list_last(list : List*) : List*
+  fun g_list_nth(list : List*, n : UInt32) : List*
+
+  # GSList
+  fun g_slist_length(list : SList*) : UInt32
+  fun g_slist_nth(list : SList*, n : UInt32) : SList*
+  fun g_slist_free(list : SList*)
+  fun g_slist_free_full(list : SList*, free_func : Proc(Void*, Nil))
+end
---
+++ src/bindings/g_object/lib_g_object.cr
@@ -0,0 +1,51 @@
+lib LibGObject
+  # Functions not declared by GObj Introspection
+
+  # We use this function to store the Crystal wrapper pointer in GObjects,
+  # So we can re-use the Crystal objects saving some memory allocations.
+  fun g_object_set_qdata(object : Void*, quark : UInt32, data : Void*)
+
+  fun g_signal_new(signal_name : LibC::Char*,
+                   itype : UInt64,
+                   signal_flags : UInt32,
+                   class_offset : UInt32,
+                   accumulator : Pointer(Void),
+                   accu_data : Pointer(Void),
+                   c_marshaller : Pointer(Void),
+                   return_type : UInt64,
+                   n_params : UInt32, ...) : UInt32
+
+  fun g_type_register_static_simple(parent_type : UInt64,
+                                    type_name : LibC::Char*,
+                                    class_size : UInt32,
+                                    class_init : ClassInitFunc,
+                                    instance_size : UInt32,
+                                    instance_init : InstanceInitFunc,
+                                    flags : Int32) : UInt64
+
+  # Param spec ref/unref
+  fun g_param_spec_ref_sink(pspec : Void*)
+  fun g_param_spec_ref(pspec : Void*)
+  fun g_param_spec_unref(pspec : Void*)
+  # Old gobject introspection libs return this typo.
+  fun g_param_spec_uref = g_param_spec_unref(pspec : Void*)
+
+  # Property related functions
+  fun g_object_get(object : Pointer(Void), property_name : Pointer(LibC::Char), ...)
+  fun g_object_set(object : Pointer(Void), property_name : Pointer(LibC::Char), ...)
+  fun g_object_new_with_properties(object_type : UInt64, n_properties : UInt32,
+                                   names : LibC::Char**, values : Value*) : Void*
+
+  # Signal related functions
+  fun g_signal_connect_data(instance : Void*,
+                            detailed_signal : UInt8*,
+                            c_handler : Void*,
+                            data : Void*,
+                            destroy_data : Void* -> Nil,
+                            flags : UInt32) : UInt64
+  @[Raises]
+  fun g_signal_emit_by_name(instance : Void*, detailed_signal : UInt8*, ...)
+
+  # Null terminated strings GType, used by GValue
+  fun g_strv_get_type : UInt64
+end
