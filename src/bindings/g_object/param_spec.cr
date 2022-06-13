68fe8576dfec663e03bd5d2a975bd23d63a239f5
Hugo Parente Lima <hugo.pl@gmail.com>
2022-06-13 16:18:48 -0300

    Raise a compile error when using `GObject::ParamSpec.g_type`.
    This also re-introduce the ParamSpec constructor methods as GObject module functions.
    
    Fix #17

--- src/bindings/g_object/param_spec.cr
+++ src/bindings/g_object/param_spec.cr
@@ -4,19 +4,9 @@ module GObject
     def ref_count : UInt32
       to_unsafe.as(Pointer(LibGObject::ParamSpec)).value.ref_count
     end
-  end
 
-  class ParamSpecInt < ParamSpec
-    # :nodoc:
-    def initialize(name : String, nick : String, blurb : String,
-                   minimum : Int32, maximum : Int32, default_value : Int32, flags : ParamFlags)
-      # This constructor will probably not exist in the future since I plan to implement some macros to declare
-      # GObject properties.
-      ptr = LibGObject.g_param_spec_int(name, nick, blurb, minimum, maximum, default_value, flags)
-      super(ptr, :full)
+    def self.g_type : UInt64
+      {% raise "GObject::ParamSpec has no GType" %}
     end
-
-    # TODO: Add getters for fields in ParamSpecInt
-    # TODO: Add a way to cast a ParamSpec and remove `ParamSpec#cast(GObject::Object)`
   end
 end
