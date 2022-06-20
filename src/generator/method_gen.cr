hugopl/gi-crystal

commit c6747cbd858f185a8ed4df7ccda0d72a43d6758b

Don't generate code for constructors with empty params.
The generic property constructors can be used instead.

src/generator/method_gen.cr
@@ -22,8 +22,10 @@ module Generator
       @crystal_arg_count = @args_strategies.size - @args_strategies.count(&.remove_from_declaration?)
     end
 
-    def ignore?
-      config.ignore?(@method.symbol)
+    def skip? : Bool
+      # We don't generate constructors with no arguments since the custom constructor can be called for this.
+      config.ignore?(@method.symbol) ||
+        (@method.flags.constructor? && @method.args.empty?)
     end
 
     def scope
@@ -38,7 +40,7 @@ module Generator
       identifier = to_identifier(@method.name)
       method_flags = @method.flags
       identifier = if method_flags.constructor?
-                     @method.name == "new" ? "initialize" : "self.#{identifier}"
+                     "self.#{identifier}"
                    elsif identifier.starts_with?("get_") && identifier.size > 4
                      identifier[4..]
                    elsif method_flags.getter? && identifier.starts_with?("is_") && identifier.size > 3
@@ -84,13 +86,7 @@ module Generator
       end
     end
 
-    private def initialize_method? : Bool
-      @method.constructor? && @method.name == "new"
-    end
-
     private def method_return_type_declaration : String
-      return "" if initialize_method?
-
       if @method.flags.constructor?
         return @method.may_return_null? ? ": self?" : ": self"
       end
@@ -177,10 +173,7 @@ module Generator
       return_type = method_return_type
 
       String.build do |s|
-        if initialize_method?
-          s << "@pointer = _retval \n" \
-               "LibGObject.g_object_set_qdata(_retval, GICrystal::INSTANCE_QDATA_KEY, Pointer(Void).new(object_id))"
-        elsif @method.constructor?
+        if @method.constructor?
           s << convert_to_crystal("_retval", @method.container.not_nil!, @method.args, :full)
         elsif return_type.is_a?(ArgInfo)
           s << to_identifier(return_type.name)

src/generator/method_holder.cr
@@ -5,7 +5,7 @@ module Generator
     macro render_methods
       each_object_method do |method|
         gen = MethodGen.new(object, method)
-        gen.generate(io) unless gen.ignore?
+        gen.generate(io) unless gen.skip?
       end
     end
 

commit 816e5bb289afbf5101fe7e3104e185e84d21d03a

Generate empty structs constructors again (#39)
Regression introduced by c6747cbd858f185a8ed4df7ccda0d72a43d6758b.

Fix https://github.com/hugopl/gtk4.cr/issues/27

src/generator/method_gen.cr
@@ -23,9 +23,7 @@ module Generator
     end
 
     def skip? : Bool
-      # We don't generate constructors with no arguments since the custom constructor can be called for this.
-      config.ignore?(@method.symbol) ||
-        (@method.flags.constructor? && @method.args.empty?)
+      config.ignore?(@method.symbol) || (@method.flags.constructor? && @method.args.empty? && object.is_a?(StructInfo))
     end
 
     def scope