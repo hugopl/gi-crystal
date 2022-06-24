2dc65ce6dfc0a05540c943bf0eb4520cb9760c49
David Keller <davidkeller@tuta.io>
2022-06-24 11:59:58 +0200

    Move cast_methods.ecr to object.cr

--- src/bindings/g_object/object.cr
+++ src/bindings/g_object/object.cr
@@ -163,5 +163,15 @@ module GObject
     def ref_count : UInt32
       to_unsafe.as(Pointer(LibGObject::Object)).value.ref_count
     end
+
+    # Cast a `GObject::Object` to `self`, throws a `TypeCastError` if the cast can't be made.
+    def self.cast(obj : GObject::Object) : self
+      cast?(obj) || raise TypeCastError.new("can't cast #{typeof(obj).name} to #{self}")
+    end
+
+    # Cast a `GObject::Object` to `self`, returns nil if the cast can't be made.
+    def self.cast?(obj : GObject::Object) : self?
+      new(obj.to_unsafe, GICrystal::Transfer::None) unless LibGObject.g_type_check_instance_is_a(obj, g_type).zero?
+    end
   end
 end
