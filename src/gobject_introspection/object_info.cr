hugopl/gi-crystal

commit ce1a86ad5b64530ea6baeeb5a5d7ac0892d6c9ae

Do not generate qdata optimized `new` method for types that doesn't support get/set qdata.

src/gobject_introspection/object_info.cr
@@ -58,13 +58,11 @@ module GObjectIntrospection
       StructInfo.new(ptr) if ptr
     end
 
-    def initially_unowned? : Bool
-      parent = LibGIRepository.g_object_info_get_parent(self)
-      return false if parent.null?
-
+    def inherits?(c_type_name : String) : Bool
+      parent = to_unsafe
       while !parent.null?
         type_name = LibGIRepository.g_object_info_get_type_name(parent)
-        if LibC.strcmp(type_name, "GInitiallyUnowned").zero?
+        if LibC.strcmp(type_name, c_type_name).zero?
           return true
         else
           new_parent = LibGIRepository.g_object_info_get_parent(parent)

ecr/object.ecr
@@ -29,7 +29,7 @@ module <%= namespace_name %>
     end
     <% end %>
 
-    GICrystal.declare_new_method(<%= type_name %>, <%= object.qdata_get_func %>, <%= object.qdata_set_func %>)
+    <% render_qdata_optimized_new_method %>
 
     <% if object.parent %>
     # Initialize a new `<%= type_name %>`.

src/generator/object_gen.cr
@@ -76,5 +76,15 @@ module Generator
         s << '/' << info.name.underscore << "\"\n"
       end
     end
+
+    macro render_qdata_optimized_new_method
+      render_qdata_optimized_new_method(io)
+    end
+
+    def render_qdata_optimized_new_method(io : IO)
+      return if !object.inherits?("GObject") && !object.inherits?("GParam")
+
+      io << "GICrystal.declare_new_method(" << type_name << ',' << object.qdata_get_func << ',' << object.qdata_set_func << ")\n"
+    end
   end
 end

commit 0a99b2e9475ef711a38bcfeef3f04d75525187b5

Fix issue introduced in ce1a86ad5b64530ea6baeeb5a5d7ac0892d6c9ae.
Just a reminder to remember how good is to not manually deal with ref counts.

src/gobject_introspection/object_info.cr
@@ -60,6 +60,8 @@ module GObjectIntrospection
 
     def inherits?(c_type_name : String) : Bool
       parent = to_unsafe
+      LibGIRepository.g_base_info_ref(parent)
+
       while !parent.null?
         type_name = LibGIRepository.g_object_info_get_type_name(parent)
         if LibC.strcmp(type_name, c_type_name).zero?