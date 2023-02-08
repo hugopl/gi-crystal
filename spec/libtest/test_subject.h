1a0797bcd3df223c5ececbb65d438e6004567ced
Hugo Parente Lima <hugo.pl@gmail.com>
2023-02-07 21:11:43 -0300

    Fix GSList objects with Modules.
    Fix #68

--- spec/libtest/test_subject.h
+++ spec/libtest/test_subject.h
@@ -292,6 +292,15 @@ GList* test_subject_return_list_of_strings_transfer_full(TestSubject* self);
 GList* test_subject_return_list_of_strings_transfer_container(TestSubject* self);
 
 /**
+ * test_subject_return_slist_of_iface_transfer_full:
+ * Returns: (transfer full) (element-type TestIface):
+ *
+ * Used to test GSList transfer full conversions, first element will be self, the second one
+ * a new created `TestSubject`.
+ */
+GSList* test_subject_return_slist_of_iface_transfer_full(TestSubject* self);
+
+/**
  * test_subject_return_slist_of_strings_transfer_full:
  * Returns: (transfer full) (element-type utf8): `["one", "two"]`
  *
--- src/bindings/g_lib/slist.cr
+++ src/bindings/g_lib/slist.cr
@@ -37,6 +37,8 @@ module GLib
 
       {% if T == String %}
         T.new(data.as(Pointer(LibC::Char)))
+      {% elsif T.module? %}
+        {{ T.name.stringify.gsub(/(.*)(::)([^:]*)\z/, "\\1::Abstract\\3").id }}.new(data, :none)
       {% else %}
         T.new(data)
       {% end %}
@@ -44,8 +46,8 @@ module GLib
 
     def finalize
       if @transfer.full?
-        {% if T > GObject::Object %}
-          LibGLib.g_slist_free_full(self, ->LibGLib.g_object_unref)
+        {% if T > GObject::Object || T.module? %}
+          LibGLib.g_slist_free_full(self, ->LibGObject.g_object_unref)
         {% else %}
           LibGLib.g_slist_free_full(self, ->LibGLib.g_free)
         {% end %}
