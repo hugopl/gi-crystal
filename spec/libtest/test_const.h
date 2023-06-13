2e3cb8a12d739def97f442f6fe2e305043acc28f
charitybell <128868596+charitybell@users.noreply.github.com>
2023-06-12 21:54:10 -0700

    Add an ignore_constants binding config setting (#94)
    Closes #87

---
+++ spec/libtest/test_const.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/**
+ * TEST_IGNORED_CONSTANT:
+ * A constant ignored in the binding config.
+ */
+#define TEST_IGNORED_CONSTANT 123
+
+/**
+ * TEST_NON_IGNORED_CONSTANT:
+ * A constant.
+ */
+#define TEST_NON_IGNORED_CONSTANT 123
+
+G_END_DECLS
