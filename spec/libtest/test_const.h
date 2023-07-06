c44b74e7ff802931a26ecdc0137523bbf5a2c30e
Hugo Parente Lima <hugo.pl@gmail.com>
2023-07-06 17:15:06 -0300

    Bind false boolean constants to false.
    Fix #108

--- spec/libtest/test_const.h
+++ spec/libtest/test_const.h
@@ -16,4 +16,16 @@ G_BEGIN_DECLS
  */
 #define TEST_NON_IGNORED_CONSTANT 123
 
+/**
+ * TEST_BOOLEAN_TRUE_CONSTANT:
+ * A boolean constant with true value
+ */
+#define TEST_BOOLEAN_TRUE_CONSTANT TRUE
+
+/**
+ * TEST_BOOLEAN_FALSE_CONSTANT:
+ * A boolean constant with false value
+ */
+#define TEST_BOOLEAN_FALSE_CONSTANT FALSE
+
 G_END_DECLS
