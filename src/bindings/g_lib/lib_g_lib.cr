9dba9154a4906c90d366cf14ad2099b7a16c4653
Hugo Parente Lima <hugo.pl@gmail.com>
2024-03-24 16:00:20 -0300

    Fix compilation with GLib 2.80. (#147)
    Fixes #145

--- src/bindings/g_lib/lib_g_lib.cr
+++ src/bindings/g_lib/lib_g_lib.cr
@@ -23,4 +23,10 @@ lib LibGLib
   # This function is used by some bindings of other modules like
   # GTK widget template and Gio GResource.
   fun g_bytes_new_static(data : Void*, size : LibC::SizeT) : Void*
+
+  # On version 2.80 GLib changed the annotation of g_once_init_enter/g_once_init_leave
+  # and added g_once_init_enter_pointer/g_once_init_leave_pointer
+  # To work with old and newer GLibs, these functiosn are lib-ignored and added here manually.
+  fun g_once_init_enter(location : Pointer(Void)) : LibC::Int
+  fun g_once_init_leave(location : Pointer(Void), result : UInt64) : Void
 end
