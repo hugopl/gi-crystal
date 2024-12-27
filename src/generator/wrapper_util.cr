b3b78295c503539929220985ea6310451ff11f36
Sylphrena <86712892+BigBoyBarney@users.noreply.github.com>
2024-12-27 22:17:30 +0100

    Fixed incomplete type declaration

--- src/generator/wrapper_util.cr
+++ src/generator/wrapper_util.cr
@@ -16,6 +16,7 @@ module Generator
     # FIXME: Possible buffer overflow... need to be sure we send a null terminated array if needed
     def generate_array_to_unsafe(io : IO, identifier : String, type : TypeInfo)
       param_type = type.param_type
+      lib_ptype = to_lib_type(param_type)
       tag = param_type.tag
 
       io << identifier << ".to_a"
@@ -26,13 +27,12 @@ module Generator
         else
           # FIXME: This is copying too much and can be optimized
           #        Main use case of this are methods receiving an array of GValues
-          lib_ptype = to_lib_type(param_type)
           io << ".map { |_i| " << ptype_name << ".new(_i).to_unsafe.as(Pointer(" << lib_ptype << ")).value }"
         end
       elsif tag.interface? || tag.utf8? || tag.filename?
         io << ".map(&.to_unsafe)"
       end
-      io << ".to_unsafe\n"
+      io << ".to_unsafe.as(Pointer(" << lib_ptype << "))\n"
     end
   end
 end
